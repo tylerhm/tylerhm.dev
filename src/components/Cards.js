0a9179a2e16f49f9a5f4d946d2ab56709b162147
Tyler Hostler-Mathis <tylerhm.dev@gmail.com>
2021-05-01 18:55:37 -0400

    Lower bandidth usage

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,37 +1,37 @@
 // Renderable cards
 const Cards = {
   'Groov': {
-    image: 'https://imgur.com/QwXe6X5l.png',
+    image: 'https://imgur.com/QwXe6X5m.png',
     body: 'A music based social media that brings freestyle to the pubic. For this project, I took the role of Project Manager and Mobile Developer.',
     page: 'https://github.com/GroovTeam',
     external: true,
   },
   'Facebook SWE Internship': {
-    image: 'https://imgur.com/pa4jxAs.png',
+    image: 'https://imgur.com/ymBJG7em.png',
     body: 'Three month paid internship on the Data Experiences team. Contributed directly to Facebook\'s codebase and bridged the gap between big data and consumers.',
     page: 'https://www.facebook.com/careers/v2/jobs/654496918442526/',
     external: true,
   },
   'Custom Portfolio': {
-    image: 'https://imgur.com/xH8SHD7l.png',
+    image: 'https://imgur.com/xH8SHD7m.png',
     body: 'Dynamic portfolio built from scratch with React and Bootstrap, complete with Google Captcha V3 and automated email forms.',
     page: 'https://beariverairions.com',
     external: true,
   },
   'Pathfinder': {
-    image: 'https://imgur.com/qc0Q9Hyl.png',
+    image: 'https://imgur.com/qc0Q9Hym.png',
     body: 'Awesome pathfinding visualization, written entirely in Javascript!',
     page: 'pathfinder',
     external: false,
   },
   'VR Molecule Builder': {
-    image: 'https://imgur.com/kQ2i8cal.png',
+    image: 'https://imgur.com/kQ2i8cam.png',
     body: 'VR application that allows the user to build organic molecules. Build in an effort to improve stereochemistry education materials.',
     page: 'https://drive.google.com/file/d/1W2QugDdjEtT40bghaI3PLtvp-3QBLKjQ/view',
     external: true,
   },
   'Predator Vs. Prey Cellular Automata': {
-    image: 'https://imgur.com/B266zePl.png',
+    image: 'https://imgur.com/B266zePm.png',
     body: 'A realtime procedural simulation of the interactions between a species of predator and prey. The beginning of my research with cellular automata.',
     page: 'predator-vs-prey',
     external: false,
